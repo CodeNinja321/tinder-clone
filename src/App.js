37c894685cb24f1989e8411f5ce71a92add7af97
Djóni Frank Strømsten <djonifrank@gmail.com>
2020-08-14 12:31:28 +0100

    added react-router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
 import React from 'react';
 import Header from './Header';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      {/* Header */}
-      {/* Card */}
-      {/* Buttons below Card */}
+      <Router>
+        <Switch>
+          <Route path="/profile">
+            <h1>Profile</h1>
+          </Route>
+          <Route path="/chat">
+            <h1>Chat</h1>
+          </Route>
+          <Route path="/">
+            <TinderCards />
+          </Route>
+        </Switch>
+        {/* Card */}
+        {/* Buttons below Card */}
 
-      {/* Chat screen */}
-      {/* Individual chat screen */}
+        {/* Chat screen */}
+        {/* Individual chat screen */}
+      </Router>
+      
     </div>
   );
 }
