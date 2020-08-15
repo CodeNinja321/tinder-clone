1352b1a0ade2423384125e90b9f19756ac31d9bb
Djóni Frank Strømsten <djonifrank@gmail.com>
2020-08-15 14:55:56 +0100

    added firebase

--- src/TinderCards.js
+++ src/TinderCards.js
@@ -1,19 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import TinderCard from 'react-tinder-card';
+import database from './firebase';
 import './TinderCards.css';
 
 function TinderCards() {
-    const [people, setPeople] = useState([
-        {
-        name: 'Steve Jobs',
-        url: 'https://upload.wikimedia.org/wikipedia/commons/d/dc/Steve_Jobs_Headshot_2010-CROP_%28cropped_2%29.jpg'
-        },
-        {
-        name: 'Mark Zuckerberg',
-        url: 'https://vvazw1o18pf4bhdd434btzh7-wpengine.netdna-ssl.com/wp-content/uploads/2020/07/Mark-Zuckerberg-Spooks-the-Internet-With-Too-Much-Sunscreen-on-His-Face-in-Hawaii-01-1.jpg'
-        },
-    ]);
+    const [people, setPeople] = useState([]);
     //const people = [];
+
+    useEffect(() => {
+        database.collection('people').onSnapshot(snapshot => (
+            setPeople(snapshot.docs.map(doc => doc.data()))
+        ))
+    }, []);
 
     return (
         <div>
