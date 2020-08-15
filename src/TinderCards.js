027ebccaffabd192853c8d985c32632d36124de2
Djóni Frank Strømsten <djonifrank@gmail.com>
2020-08-15 12:44:55 +0100

    a

--- src/TinderCards.js
+++ src/TinderCards.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 
 function TinderCards() {
+    const [people, setPeople] = useStateate([]);
+    //const people = [];
+
     return (
         <div>
-            
+            <h1>Tinder Cards</h1>
         </div>
     )
 }
